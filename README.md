f3ebf936d85d4fe2ae3ebbe682983a13e29f2838
Yoav <yoav@gingerlime.com>
2022-07-23 16:55:01 +0200

    Download-cookies initial version
    * based on https://stackoverflow.com/a/50099635/305019
      and https://github.com/mdn/webextensions-examples/tree/master/list-cookies
      (including icon)
    * downloads all cookies from the active tab as cookies.json
    * TODO:
      - still uses manifest v2 (upgrade to v3?)
      - uses popup, but there's no need for it?
      - add localStorage data
      - check license for icon, or replace with a suitable licensed icon

---
+++ README.md
@@ -0,0 +1,5 @@
+# download-cookies
+
+## What it does
+
+This Chrome extension downloads the cookies for the active browser tab in JSON format.
---
+++ cookies.js
@@ -0,0 +1,31 @@
+// based on: https://stackoverflow.com/a/50099635/305019
+chrome.tabs.executeScript({
+  code: 'performance.getEntriesByType("resource").map(e => e.name)',
+}, data => {
+  if (chrome.runtime.lastError || !data || !data[0]) return;
+  const urls = data[0].map(url => url.split(/[#?]/)[0]);
+  const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
+  Promise.all(
+    uniqueUrls.map(url =>
+      new Promise(resolve => {
+        chrome.cookies.getAll({url}, resolve);
+      })
+    )
+  ).then(results => {
+    // convert the array of arrays into a deduplicated flat array of cookies
+    const cookies = [
+      ...new Map(
+        [].concat(...results)
+          .map(c => [JSON.stringify(c), c])
+      ).values()
+    ];
+
+    // do something with the cookies here
+    console.log(uniqueUrls, cookies);
+
+    // download the cookies to a cookies.json file
+    var blob = new Blob([JSON.stringify(cookies, null, 1)], {type: "application/json"});
+    var url = URL.createObjectURL(blob);
+    chrome.downloads.download({url, filename: "cookies.json"});
+ });
+});
